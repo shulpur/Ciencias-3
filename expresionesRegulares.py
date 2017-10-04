8f1e90813245940d00e56ba722558d1f0fd257a9
shulpur <rubencas20@gmail.com>
2017-10-04 05:45:19 -0500

    Add files via upload

---
+++ expresionesRegulares.py
@@ -0,0 +1,28 @@
+import re
+p = re.compile('ab*')
+m = p.match('ab')
+if m:
+    print('Match found: ', m.group())
+else:
+    print('No match')
+
+
+lista = ['1','1','1','+','-','mi_Casa22','=','%']
+listaSalida = []
+valor = re.compile('[0-9]')
+operacion = re.compile('[\+\-\*\/\=]')
+variable = re.compile('^[a-z][a-zA-Z0-9_]')
+
+for i in lista:
+    if valor.match(i):
+        listaSalida.append(['valor', i])
+    elif operacion.match(i):
+        listaSalida.append(['operacion', i])
+    elif variable.match(i):
+        listaSalida.append(['variable', i])
+    else:
+        print('error tocken no valido: ', i)
+
+print(listaSalida)
+
+
